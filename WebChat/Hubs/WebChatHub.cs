64802f441b1a3142cc6ef184b3a6e6c086c5062d
raycdut <raycdut@hotmail.com>
2017-05-08 22:15:19 +0800

    add signalr hub

---
+++ WebChat/Hubs/WebChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebChat.Hubs
+{
+    public class WebChatHub : Hub
+    {
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+    }
+}
